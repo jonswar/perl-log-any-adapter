97bbf1ca28dff424ad631138d96f9b9d05e0eb91
Jonathan Swartz <swartz@pobox.com>
2009-10-27 22:03:36 -0700

    updating to next version 0.03

--- lib/Log/Any/Adapter.pm
+++ lib/Log/Any/Adapter.pm
@@ -6,7 +6,7 @@ use Log::Any::Adapter::Util qw(make_method);
 use strict;
 use warnings;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 # Checked by Log::Any to see if get_logger should be forwarded here
 #
